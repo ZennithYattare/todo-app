d93f030f06d867308f836a2d5984d0600099a86e
ZennithYattare <luis.espera.0758@gmail.com>
2024-03-01 14:50:52 +0800

    chore: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+	return (
+		<>
+			<header className="div-header">
+				<div className="div-header-child">
+					<h1 className="header-weight-600">LexMeet</h1>
+				</div>
+			</header>
+		</>
+	);
+};
+
+export default Header;
