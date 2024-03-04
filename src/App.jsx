a5ba2b02e39e884cd179e6e257c1ef2a0d851ecb
ZennithYattare <luis.espera.0758@gmail.com>
2024-03-04 16:01:56 +0800

    chore: markAsPending function and separate pending from done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 import Header from "./components/Header";
 import ModalAdd from "./components/ModalAdd";
@@ -26,6 +28,35 @@ function App() {
 		deleteTodo(id);
 
 		setTodos(getAllTodos());
+	};
+
+	const markAsPending = () => {
+		// Get the existing todos from localStorage
+		const existingTodos = JSON.parse(localStorage.getItem("notes")) || [];
+
+		// Mark all todos in todoArray as pending
+		const updatedTodos = existingTodos.map((todo) => {
+			if (todoArray.includes(todo.id)) {
+				const updatedTodo = {
+					...todo,
+					updated_at: new Date().toISOString(),
+					completed: false,
+				};
+				editTodo(updatedTodo);
+				return updatedTodo;
+			} else {
+				return todo;
+			}
+		});
+
+		// Save the updated todos back to localStorage
+		localStorage.setItem("notes", JSON.stringify(updatedTodos));
+
+		// Update the state with the updated todos
+		setTodos(updatedTodos);
+
+		// Clear todoArray
+		setTodoArray([]);
 	};
 
 	const markAsDone = () => {
@@ -91,95 +122,177 @@ function App() {
 				onHide={() => setShowEditModal(false)}
 			/>
 
-			<Button variant="primary" onClick={() => setShowAddModal(true)}>
-				Add Todo
-			</Button>
+			<Tabs
+				defaultActiveKey="pending"
+				id="uncontrolled-tab-example"
+				className="mb-3"
+			>
+				<Tab eventKey="pending" title="Pending">
+					<Button
+						variant="primary"
+						onClick={() => setShowAddModal(true)}
+					>
+						Add Todo
+					</Button>
 
-			<Button variant="success" onClick={markAsDone}>
-				Mark as Done
-			</Button>
-
-			<div className="">
-				{todos.map((todo) => {
-					if (!todo.completed) {
-						return (
-							<div key={todo.id}>
-								<Form>
-									<Form.Check
-										type="checkbox"
-										label={todo.title}
-										onChange={(e) => {
-											if (e.target.checked) {
-												// If the checkbox is checked, add the todo.id to arrayTodo
-												setTodoArray([
-													...todoArray,
-													todo.id,
-												]);
-											} else {
-												// If the checkbox is unchecked, remove the todo.id from arrayTodo
-												setTodoArray(
-													todoArray.filter(
-														(id) => id !== todo.id
-													)
-												);
-											}
-										}}
-									/>
-								</Form>
-								<h4>{todo.id}</h4>
-								<h3>{todo.title}</h3>
-								<h2>
-									{todo.completed ? "completed" : "pending"}
-								</h2>
-								<p>{todo.description}</p>
-								<p>
-									{"Due on: "}
-									{new Date(todo.deadline).toLocaleDateString(
-										"en-US",
-										{
+					<Button variant="success" onClick={markAsDone}>
+						Mark as Done
+					</Button>
+					{todos.map((todo) => {
+						if (!todo.completed) {
+							return (
+								<div key={todo.id}>
+									<Form>
+										<Form.Check
+											type="checkbox"
+											label={todo.title}
+											onChange={(e) => {
+												if (e.target.checked) {
+													// If the checkbox is checked, add the todo.id to arrayTodo
+													setTodoArray([
+														...todoArray,
+														todo.id,
+													]);
+												} else {
+													// If the checkbox is unchecked, remove the todo.id from arrayTodo
+													setTodoArray(
+														todoArray.filter(
+															(id) =>
+																id !== todo.id
+														)
+													);
+												}
+											}}
+										/>
+									</Form>
+									<h3>{todo.title}</h3>
+									<p>{todo.description}</p>
+									<p>
+										{"Due on: "}
+										{new Date(
+											todo.deadline
+										).toLocaleDateString("en-US", {
 											year: "numeric",
 											month: "long",
 											day: "numeric",
-										}
-									)}
-									{" at "}
-									{new Date(todo.deadline).toLocaleTimeString(
-										"en-US",
-										{
+										})}
+										{" at "}
+										{new Date(
+											todo.deadline
+										).toLocaleTimeString("en-US", {
 											hour: "2-digit",
 											minute: "2-digit",
 											hour12: true,
-										}
-									)}
-								</p>
+										})}
+									</p>
 
-								<Button
-									variant="primary"
-									onClick={() => {
-										if (todo) {
-											handleUpdate(todo.id);
-										}
-									}}
-								>
-									View/Edit
-								</Button>
-								<Button
-									variant="danger"
-									onClick={() => {
-										if (todo) {
-											handleDelete(todo.id);
-										}
-									}}
-								>
-									Delete
-								</Button>
-							</div>
-						);
-					} else {
-						return null;
-					}
-				})}
-			</div>
+									<Button
+										variant="primary"
+										onClick={() => {
+											if (todo) {
+												handleUpdate(todo.id);
+											}
+										}}
+									>
+										View/Edit
+									</Button>
+									<Button
+										variant="danger"
+										onClick={() => {
+											if (todo) {
+												handleDelete(todo.id);
+											}
+										}}
+									>
+										Delete
+									</Button>
+								</div>
+							);
+						}
+					})}
+				</Tab>
+				<Tab eventKey="done" title="Done">
+					<Button variant="success" onClick={markAsPending}>
+						Mark as Pending
+					</Button>
+					{todos.map((todo) => {
+						if (todo.completed) {
+							return (
+								<div key={todo.id}>
+									<Form>
+										<Form.Check
+											type="checkbox"
+											label={todo.title}
+											onChange={(e) => {
+												if (e.target.checked) {
+													// If the checkbox is checked, add the todo.id to arrayTodo
+													setTodoArray([
+														...todoArray,
+														todo.id,
+													]);
+												} else {
+													// If the checkbox is unchecked, remove the todo.id from arrayTodo
+													setTodoArray(
+														todoArray.filter(
+															(id) =>
+																id !== todo.id
+														)
+													);
+												}
+											}}
+										/>
+									</Form>
+									<h3 className="strike-through">
+										{todo.title}
+									</h3>
+									<p className="strike-through">
+										{todo.description}
+									</p>
+									<p>
+										{"Due on: "}
+										{new Date(
+											todo.deadline
+										).toLocaleDateString("en-US", {
+											year: "numeric",
+											month: "long",
+											day: "numeric",
+										})}
+										{" at "}
+										{new Date(
+											todo.deadline
+										).toLocaleTimeString("en-US", {
+											hour: "2-digit",
+											minute: "2-digit",
+											hour12: true,
+										})}
+									</p>
+
+									<Button
+										variant="primary"
+										onClick={() => {
+											if (todo) {
+												handleUpdate(todo.id);
+											}
+										}}
+									>
+										View/Edit
+									</Button>
+									<Button
+										variant="danger"
+										onClick={() => {
+											if (todo) {
+												handleDelete(todo.id);
+											}
+										}}
+									>
+										Delete
+									</Button>
+								</div>
+							);
+						}
+					})}
+				</Tab>
+			</Tabs>
 		</>
 	);
 }
